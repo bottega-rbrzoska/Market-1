7e42007b730ca0ad16de74bf2679834f45b8eebe
Piotr Gankiewicz <spetz90@gmail.com>
2019-06-25 15:24:38 +0200

    Products tests

--- tests/Market.Tests.Integration/Controllers/ProductsControllerTests.cs
+++ tests/Market.Tests.Integration/Controllers/ProductsControllerTests.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Market.Application.Products.Commands;
 using Market.Application.Products.DTO;
 using Market.Web;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +35,32 @@ namespace Market.Tests.Integration.Controllers
             var content = await response.Content.ReadAsStringAsync();
             var products = JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(content);
             products.Should().BeEmpty();
+        }
+        
+        [Fact]
+        public async Task add_product_should_succeed()
+        {
+            var httpClient = _factory.CreateClient();
+            var command = new CreateProduct(Guid.Empty, "Huawei", "phones",
+                "Huawei phone", 3500);
+            var request = new StringContent(
+                JsonConvert.SerializeObject(command), Encoding.UTF8,
+                "application/json");
+
+            var response = await httpClient.PostAsync("api/products", request);
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            var productUrl = response.Headers.Location?.ToString();
+            productUrl.Should().NotBeEmpty();
+
+            var getResponse = await httpClient.GetAsync(productUrl);
+
+            getResponse.EnsureSuccessStatusCode();
+            var content = await getResponse.Content.ReadAsStringAsync();
+            var product = JsonConvert.DeserializeObject<ProductDetailsDto>(content);
+            product.Should().NotBeNull();
+            product.Name.Should().Be(command.Name);
         }
     }
 }
