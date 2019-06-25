c6f46f85ed420cd50caf5013957ed7ecf663995f
Piotr Gankiewicz <spetz90@gmail.com>
2019-06-25 15:08:44 +0200

    Products tests

--- tests/Market.Tests.Integration/Controllers/ProductsControllerTests.cs
+++ tests/Market.Tests.Integration/Controllers/ProductsControllerTests.cs
@@ -1,7 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Market.Application.Products.DTO;
+using Market.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Xunit;
+
 namespace Market.Tests.Integration.Controllers
 {
-    public class ProductsControllerTests
+    public class ProductsControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        // GET /api/products - []
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ProductsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory.WithWebHostBuilder(b => b.UseEnvironment("test"));
+        }
+
+        [Fact]
+        public async Task get_products_should_return_empty_collection()
+        {
+            var httpClient = _factory.CreateClient();
+
+            var response = await httpClient.GetAsync("api/products");
+
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var products = JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(content);
+            products.Should().BeEmpty();
+        }
     }
 }
