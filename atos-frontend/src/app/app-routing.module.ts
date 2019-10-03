8be0d31d637e7c1a7ad96daba5209f0ef8861b0a
Rafal Brzoska <rafal@brzoska.net.pl>
2019-10-03 15:20:15 +0200

    Users link

--- atos-frontend/src/app/app-routing.module.ts
+++ atos-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'test', component: TestComponent }
+  { path: 'test', component: TestComponent },
+  { path: 'users', component: UserListComponent }
 ];
 
 @NgModule({
