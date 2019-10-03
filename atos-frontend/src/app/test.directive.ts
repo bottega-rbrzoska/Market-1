4ee4ba1d1e1b54df6b9e15e28b74ede64e0b899e
Rafal Brzoska <rafal@brzoska.net.pl>
2019-10-03 16:56:24 +0200

    Hover directive

--- atos-frontend/src/app/test.directive.ts
+++ atos-frontend/src/app/test.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTest]'
 })
 export class TestDirective {
 
+  @Input() appTest;
+  @Input('appTestHoverOut') hoverOutColor;
 
   constructor(private elRef: ElementRef) {
     elRef.nativeElement.style.color = 'blue';
@@ -12,13 +14,17 @@ export class TestDirective {
 
   @HostListener('mouseover')
   onHover() {
-    this.elRef.nativeElement.style.backgroundColor = 'black';
+    this.elRef.nativeElement.style.backgroundColor = this.appTest || 'black';
   }
 
 
   @HostListener('mouseout')
   onHoverOut() {
-    this.elRef.nativeElement.style.backgroundColor = 'transparent';
+    this.elRef.nativeElement.style.backgroundColor = this.hoverOutColor || 'transparent';
+  }
+
+  ngOnChanges(changes) {
+    console.log(changes)
   }
 
 }
