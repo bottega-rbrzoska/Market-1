3ec014ac265fafab125fa26c06ae8db53a86c822
Rafal Brzoska <rafal@brzoska.net.pl>
2019-10-04 11:26:03 +0200

    users api

--- atos-frontend/src/app/user/user.service.ts
+++ atos-frontend/src/app/user/user.service.ts
@@ -4,18 +4,7 @@ import { HomeUser } from '../models/home-user.interface';
 @Injectable()
 export class UserService {
 
-  private userList: HomeUser[] = [
-    {
-      name: 'Alojzy',
-      age: 67,
-      address: { city: 'Opole', street: 'Wrocławska' }
-    },
-    {
-      name: 'Stefan',
-      age: 76,
-      address: { city: 'Wrocław', street: 'Opolska' }
-    }
-  ];
+  private userList: HomeUser[] = [];
   constructor() { }
 
   getUsersList() {
