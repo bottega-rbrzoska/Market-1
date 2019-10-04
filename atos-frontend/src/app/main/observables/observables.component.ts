41927a6eb8478ed50aa1867e27e93a97fa5df423
Rafal Brzoska <rafal@brzoska.net.pl>
2019-10-04 11:01:52 +0200

    Observables part 1

--- atos-frontend/src/app/main/observables/observables.component.ts
+++ atos-frontend/src/app/main/observables/observables.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
+import { tap, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observables',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./observables.component.scss']
 })
 export class ObservablesComponent implements OnInit {
+  currentSubjValue = '';
+  obs = new Observable<number>(observer => {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.error('Error!');
+    observer.next(4);
+  });
 
+  subject = new Subject<string>();
+  bSubject = new BehaviorSubject('x');
+  ofObs = of(1,2,3,4);
   constructor() { }
 
   ngOnInit() {
+    this.bSubject.next('xx');
+    console.log(this.bSubject.value)
+    this.bSubject.subscribe(console.log);
+    this.ofObs.subscribe(console.log)
+    this.obs.subscribe(
+      (nextVal) =>  console.log(nextVal),
+      (error) => console.error(error),
+      () =>  console.log('complete!') );
+
+    this.obs.subscribe(
+      {
+        next: (nextVal) =>  console.log(nextVal),
+        error:  (error) => console.error(error),
+        complete: () =>  console.log('complete!')
+      } );
+
+    this.subject
+    .pipe(
+      filter(val => val.includes('a')),
+      tap((val) => this.currentSubjValue = val),
+      map(val => val.toUpperCase())
+    )
+    .subscribe(console.log);
+
+    this.subject.next('1abc');
+    this.subject.next('2bc');
+    this.subject.next('3c');
   }
 
 }
--- atos-frontend/src/app/products/product.service.ts
+++ atos-frontend/src/app/products/product.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product.interface';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class ProductService {
+  private products: Product[] = [
+    { id: '1', name: 'pomidor', description: 'opis pomidora', price: 1.99, category: 'warzywo'},
+    { id: '2', name: 'ogorek', description: 'costam o ogorku', price: 0.99, category: 'warzywo'}
+  ];
+  private productsSubj = new BehaviorSubject<Product[]>(this.products);
 
+  products$ = this.productsSubj.asObservable();
   constructor() { }
 }
